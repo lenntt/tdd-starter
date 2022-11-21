b16d7f64e7f9ff1986cb70979e26470d52a29472
Lennart Tange <lennarttange@gmail.com>
2022-11-21 08:14:24 +0100

    cleanup

--- tddiscipline/index.js
+++ tddiscipline/index.js
@@ -1,15 +1,12 @@
 #!/usr/bin / env node
 
-const { exec, spawn } = require("child_process");
+const { spawn } = require("child_process");
+const fs = require('fs');
 
 const args = process.argv.slice(2);
-const command = args[0];
-
-const fs = require('fs');
 
 const RESULT_FILE = '.tddiscipline.result.json';
 
-// cmd = spawn(command, args, { stdio: 'inherit' });
 cmd = spawn(args[0], args.slice(1), { stdio: 'inherit' });
 cmd.on('exit', (code) => {
     const dot = code === 0 ? '🟢' : (code > 1 ? '🟡' : '🔴');
@@ -18,7 +15,6 @@ cmd.on('exit', (code) => {
     if (fs.existsSync(RESULT_FILE)) {
         oldResults = JSON.parse(fs.readFileSync(RESULT_FILE));
     }
-
     saveResults(oldResults, dot);
 });
 
