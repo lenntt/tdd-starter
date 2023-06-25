04fd88f6aefa415c6636b9f14d751dcf3f708618
Jim Bloemkolk <jimbloemkolk@gmail.com>
2023-06-25 21:53:56 +0200

    extend beginstate with inital code to handle stream

---
+++ net/Result/Program.cs
@@ -0,0 +1,9 @@
+﻿using ServiceStack;
+
+var client = new ServerEventsClient("https://tdd-bowling-alcckkju2q-ez.a.run.app/");
+client.Start();
+
+while (true) {
+    var message = await client.WaitForNextMessage();
+    Console.WriteLine(message.Data);
+}
